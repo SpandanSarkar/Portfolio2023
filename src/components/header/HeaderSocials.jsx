7756a80e7c1c0f7c89de22402c1b07a1646772b2
Spandan Sarkar <spandansarkar71@gmail.com>
2023-04-05 23:55:27 +0600

    Header Section Modified

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -4,7 +4,7 @@ import {SiLeetcode} from 'react-icons/si'
 import {BsGithub} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
-    <div className='hear__socials'>
+    <div className='header__socials'>
         <a href="https://www.linkedin.com/in/spandan-sarkar-690a57174" target="_blank"><BsLinkedin/></a>
         <a href="https://leetcode.com/user0904tF/" target="_blank"><SiLeetcode/></a>
         <a href="https://github.com/SpandanSarkar" target="_blank"><BsGithub/></a>
