5dad06964cd26190be4c785dcb67b40619735a31
Spandan Sarkar <[spandansarkar71@gmail.com]>
2023-06-20 17:32:30 +0600

    Icon Changed

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 import './experience.css'
 import {BsFillClipboard2CheckFill} from 'react-icons/bs'
+import {AiFillHtml5} from 'react-icons/ai'
+import {DiCss3} from 'react-icons/di'
+import {DiJavascript1} from 'react-icons/di'
+import {TbBrandTypescript} from 'react-icons/tb'
+import {FaAngular} from 'react-icons/fa'
+import {FaReact} from 'react-icons/fa'
+import {SiDotnet} from 'react-icons/si'
+import {DiNodejsSmall} from 'react-icons/di'
+import {DiJava} from 'react-icons/di'
+import {SiOracle} from 'react-icons/si'
+import {DiMongodb} from 'react-icons/di'
+import {SiMysql} from 'react-icons/si'
 
 const Experience = () => {
   return (
@@ -12,42 +24,42 @@ const Experience = () => {
           <h3>Frontend Development</h3>
           <div className="experience__content">
             <article className='experience__details'>
-              <BsFillClipboard2CheckFill className='experience__details-icon'/>
+              <AiFillHtml5 className='experience__details-icon'/>
               <div>
                 <h4>HTML</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
             <article className='experience__details'>
-              <BsFillClipboard2CheckFill className='experience__details-icon'/>
+              <DiCss3 className='experience__details-icon'/>
               <div>
                 <h4>CSS</h4>
                 <small className='text-light'>Professional</small>
               </div>
             </article>
             <article className='experience__details'>
-              <BsFillClipboard2CheckFill className='experience__details-icon'/>
+              <DiJavascript1 className='experience__details-icon'/>
               <div>
                 <h4>JavaScript</h4>
                 <small className='text-light'>Professional</small>
               </div>
             </article>
             <article className='experience__details'>
-              <BsFillClipboard2CheckFill className='experience__details-icon'/>
+              <TbBrandTypescript className='experience__details-icon'/>
               <div>
                 <h4>TypeScript</h4>
                 <small className='text-light'>Professional</small>
               </div>
             </article>
             <article className='experience__details'>
-              <BsFillClipboard2CheckFill className='experience__details-icon'/>
+              <FaAngular className='experience__details-icon'/>
               <div>
                 <h4>Angular</h4>
                 <small className='text-light'>Professional</small>
               </div>
             </article>
             <article className='experience__details'>
-              <BsFillClipboard2CheckFill className='experience__details-icon'/>
+              <FaReact className='experience__details-icon'/>
               <div>
                 <h4>React</h4>
                 <small className='text-light'>Intermediate</small>
@@ -60,42 +72,42 @@ const Experience = () => {
           <h3>Backend Development</h3>
           <div className="experience__content">
             <article className='experience__details'>
-              <BsFillClipboard2CheckFill className='experience__details-icon'/>
+              <SiDotnet className='experience__details-icon'/>
               <div>
                 <h4>ASP.NET Core</h4>
                 <small className='text-light'>Professional</small>
               </div>
             </article>
             <article className='experience__details'>
-              <BsFillClipboard2CheckFill className='experience__details-icon'/>
+              <DiNodejsSmall className='experience__details-icon'/>
               <div>
                 <h4>Node JS</h4>
                 <small className='text-light'>Intermediate</small>
               </div>
             </article>
             <article className='experience__details'>
-              <BsFillClipboard2CheckFill className='experience__details-icon'/>
+              <DiJava className='experience__details-icon'/>
               <div>
                 <h4>Java</h4>
-                <small className='text-light'>Problem Solving Language</small>
+                <small className='text-light'>Problem Solving</small>
               </div>
             </article>
             <article className='experience__details'>
-              <BsFillClipboard2CheckFill className='experience__details-icon'/>
+              <SiOracle className='experience__details-icon'/>
               <div>
                 <h4>Oracle</h4>
                 <small className='text-light'>Professional</small>
               </div>
             </article>
             <article className='experience__details'>
-              <BsFillClipboard2CheckFill className='experience__details-icon'/>
+              <DiMongodb className='experience__details-icon'/>
               <div>
                 <h4>MongoDB</h4>
                 <small className='text-light'>Intermediate</small>
               </div>
             </article>
             <article className='experience__details'>
-              <BsFillClipboard2CheckFill className='experience__details-icon'/>
+              <SiMysql className='experience__details-icon'/>
               <div>
                 <h4>MySQL</h4>
                 <small className='text-light'>Intermediate</small>
