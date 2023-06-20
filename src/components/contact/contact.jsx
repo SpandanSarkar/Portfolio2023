09480054a983b8c38d6b1266f65264eb3735b1e1
Spandan Sarkar <[spandansarkar71@gmail.com]>
2023-06-20 15:52:43 +0600

    Done with website

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,68 +1,113 @@
-import React from 'react'
-import './contact.css'
-import {SiGmail} from 'react-icons/si'
-import {FaLinkedinIn} from 'react-icons/fa'
-import {FaWhatsapp} from 'react-icons/fa'
+import React from "react";
+import "./contact.css";
+import { SiGmail } from "react-icons/si";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
-import { useRef } from 'react';
-import emailjs from 'emailjs-com'
+import { useRef } from "react";
+import emailjs from "emailjs-com";
 
 
 const ContactUs = () => {
+  const MySwal = withReactContent(Swal)
   const form = useRef();
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_q9ilso9', 'template_jggcxe6', form.current, 'ABr4Isr3V4LBkd11V')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
+    emailjs
+      .sendForm(
+        "service_q9ilso9",
+        "template_jggcxe6",
+        form.current,
+        "ABr4Isr3V4LBkd11V"
+      )
+      .then(
+        (result) => {
+          Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Your Message is received!',
+            showConfirmButton: false,
+            timer: 2500
+          })
+        },
+        (error) => {
+          MySwal.fire({
+            title: <strong>Message is not sent!</strong>,
+            html: <i>Please Try Again</i>,
+            icon: 'error'
+          })
+        }
+      );
 
-      e.target.reset()
+    e.target.reset();
   };
 
   return (
-    <section id='contact'>
+    <section id="contact">
       <h5>Get In Touch</h5>
       <h2>Contanct Me</h2>
 
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <SiGmail className='contact__option-icon'/>
+            <SiGmail className="contact__option-icon" />
             <h4>Email</h4>
             <h5>spandan.99sarkar@gmail.com</h5>
-            <a href="https://www.gmail.com" target='_blank' rel="noreferrer">Send an Email</a>
+            <a href="https://www.gmail.com" target="_blank" rel="noreferrer">
+              Send an Email
+            </a>
           </article>
 
           <article className="contact__option">
-            <FaLinkedinIn className='contact__option-icon'/>
+            <FaLinkedinIn className="contact__option-icon" />
             <h4>LinkedIn</h4>
             <h5>Spandan Sarkar</h5>
-            <a href="https://www.linkedin.com/in/spandan-sarkar-690a57174/" target='_blank' rel="noreferrer">View Profile</a>
+            <a
+              href="https://www.linkedin.com/in/spandan-sarkar-690a57174/"
+              target="_blank"
+              rel="noreferrer">
+              View Profile
+            </a>
           </article>
 
           <article className="contact__option">
-            <FaWhatsapp className='contact__option-icon'/>
+            <FaWhatsapp className="contact__option-icon" />
             <h4>WhatsApp</h4>
             <h5>01673778899</h5>
-            <a href="https://api.whatsapp.com/send?phone=01673778899" target='_blank' rel="noreferrer">WhatsApp Me</a>
+            <a
+              href="https://api.whatsapp.com/send?phone=01673778899"
+              target="_blank"
+              rel="noreferrer">
+              WhatsApp Me
+            </a>
           </article>
         </div>
-        
+
         {/* End of Contact Options */}
 
         <form ref={form} onSubmit={sendEmail}>
-          <input type="text" name='name' placeholder='Your Full Name' required/>
-          <input type="email" name='email' placeholder='Your Email' required/>
-          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
-          <button type='submit' className='btn btn-primary'>Send Message</button>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
         </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default ContactUs
+export default ContactUs;
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
+import { BsLinkedin } from "react-icons/bs";
+import { SiLeetcode } from "react-icons/si";
+import { BsGithub } from "react-icons/bs";
+
 const footer = () => {
   return (
-    <div>footer</div>
-  )
-}
+    <footer>
+      <a href="#" className="footer__logo">
+        Spandan Sarkar
+      </a>
 
-export default footer
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contacts</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a
+          href="https://www.linkedin.com/in/spandan-sarkar-690a57174"
+          target="_blank">
+          <BsLinkedin />
+        </a>
+        <a href="https://leetcode.com/user0904tF/" target="_blank">
+          <SiLeetcode />
+        </a>
+        <a href="https://github.com/SpandanSarkar" target="_blank">
+          <BsGithub />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Spandan Sarkar. All rights reserved.</small>
+      </div>
+    </footer>
+  );
+};
+
+export default footer;
